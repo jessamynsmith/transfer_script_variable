596675fd07ee25491f7cdf4be8fd8725f9e30718
jessamynsmith <jessamyn.smith@gmail.com>
2017-08-14 15:13:19 -0400

    Made ajax work

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET', 'POST'])
 def display():
-    return render_template('test.html')
+    return render_template('html_with_javascript.html')
 
 
-@app.route('/hello', methods=['GET', 'POST'])
-def background():
-    var = request.form('name')
-    message = "You are " + var
-    print(message)
-    return render_template('html_with_javascript.html', message=message)
+@app.route('/hello', methods=['POST'])
+def post():
+    # Get the parsed contents of the form data
+    json = request.get_json()
+    print(json)
+    return jsonify(json)
